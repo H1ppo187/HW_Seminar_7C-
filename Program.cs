8a4e1ad8c494aa907c02d3839f71d9297b24879f
Alexander Balashov <balashov187@yandex.ru>
2023-03-30 21:55:42 +0300

    Задача 520

--- Program.cs
+++ Program.cs
@@ -57,17 +57,65 @@
 // i = 4, j = 2 -> такого элемента в массиве нет
 
 
-int[,] array = GetArray(5,5, 0, 10);
+// int[,] array = GetArray(5,5, 0, 10);
+// PrintArray(array);
+
+// Console.Write("Введите номер строки: ");
+// int i = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите номер столбца ");
+// int j = int.Parse(Console.ReadLine()!);
+
+// FindElement(array, i,j);
+
+
+// // ----------------Заполнение массива
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] res = new int[m,n];
+
+//     for (int i = 0; i < m; i++)
+//     {
+//         for(int j = 0; j < n; j++){
+//             res[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return res;
+// }
+
+// // -----------------Вывод массива-----------------
+// void PrintArray(int[,] array){
+//     for(int i = 0; i < array.GetLength(0); i++){
+//         for(int j = 0; j < array.GetLength(1); j++){
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// // ---------------поиск элемента---------------
+// void FindElement(int[,] array, int i, int j)
+// {
+  
+//     if (i > array.GetLength(0) || j > array.GetLength(1))
+//     {
+//         Console.WriteLine("Такого элемента нет");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Такой элемент есть {array[i,j]}");
+//     }
+// }
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = GetArray(3,4, 0, 10);
 PrintArray(array);
-
-Console.Write("Введите номер строки: ");
-int i = int.Parse(Console.ReadLine()!);
-
-Console.Write("Введите номер столбца ");
-int j = int.Parse(Console.ReadLine()!);
-
-FindElement(array, i,j);
-
+Average(array);
 
 // ----------------Заполнение массива
 int[,] GetArray(int m, int n, int minValue, int maxValue)
@@ -92,16 +140,16 @@ void PrintArray(int[,] array){
         Console.WriteLine();
     }
 }
-// ---------------поиск элемента---------------
-void FindElement(int[,] array, int i, int j)
+// -----------------среднее арифметическое в столбце-----------------
+void Average(int[,] newArray)
 {
-  
-    if (i > array.GetLength(0) || j > array.GetLength(1))
+    for (int j = 0; j < newArray.GetLength(1); j++)
     {
-        Console.WriteLine("Такого элемента нет");
-    }
-    else
-    {
-        Console.WriteLine($"Такой элемент есть {array[i,j]}");
+        double sum = 0;
+        for (int i = 0; i < newArray.GetLength(0); i++)
+        {
+            sum += newArray[i, j];
+        }
+        Console.Write($"{sum / newArray.GetLength(0)}; ");
     }
 }
